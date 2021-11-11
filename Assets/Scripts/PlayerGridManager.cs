06c1f9ceb8acf75900759599ce6790b6435dd2b8
Jawad Usman <jwdusmn@gmail.com>
2021-11-10 19:05:46 -0500

    Grid updates in scene
    - assigned grid moving functions to unity buttons
    - grid changes now visible and update in scene
    - fixed some of the grid moving functions

--- Assets/Scripts/PlayerGridManager.cs
+++ Assets/Scripts/PlayerGridManager.cs
@@ -7,22 +7,126 @@ public class PlayerGridManager : MonoBehaviour
 {
     public bool[,] playerGrid;
 
-    [SerializeField] List<Vector2> playerGridPositions;
-    [SerializeField] Button buttonRightTop, ButtonRightMid, ButtonLeftBot;
+    [SerializeField] List<Transform> playerGridPositions;
+    [SerializeField] List<Image> gridSprites;
+
     // Start is called before the first frame update
     void Start()
     {
+
         playerGrid = new bool[3, 3];
         playerGrid[0, 0] = true;
         playerGrid[0, 2] = true;
         playerGrid[2, 0] = true;
         playerGrid[2, 2] = true;
+        GridPositionUpdater();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void GridPositionUpdater()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                if(playerGrid[i, j])
+                {
+                    gridSprites[(i * 3) + j].color = Color.red;
+                }
+                else
+                {
+                    gridSprites[(i * 3) + j].color = Color.white;
+                }
+            }
+        }
+
+        //if (playerGrid[0, 0])
+        //{
+        //    gridSprites[0].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[0].color = Color.white;
+        //}
+
+        //if (playerGrid[0, 1])
+        //{
+        //    gridSprites[1].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[1].color = Color.white;
+        //}
+
+        //if (playerGrid[0, 2])
+        //{
+        //    gridSprites[2].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[2].color = Color.white;
+        //}
+
+        //if (playerGrid[1, 0])
+        //{
+        //    gridSprites[3].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[3].color = Color.white;
+        //}
+
+        //if (playerGrid[1, 1])
+        //{
+        //    gridSprites[4].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[4].color = Color.white;
+        //}
+
+        //if (playerGrid[1, 2])
+        //{
+        //    gridSprites[5].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[5].color = Color.white;
+        //}
+
+        //if (playerGrid[2, 0])
+        //{
+        //    gridSprites[6].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[6].color = Color.white;
+        //}
+
+        //if (playerGrid[2, 1])
+        //{
+        //    gridSprites[7].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[7].color = Color.white;
+        //}
+
+        //if (playerGrid[2, 2])
+        //{
+        //    gridSprites[8].color = Color.red;
+        //}
+        //else
+        //{
+        //    gridSprites[8].color = Color.white;
+        //}
+
     }
 
     //move right
@@ -32,6 +136,8 @@ public class PlayerGridManager : MonoBehaviour
         playerGrid[0, 2] = playerGrid[0, 1];
         playerGrid[0, 1] = playerGrid[0, 0];
         playerGrid[0, 0] = temp;
+
+        GridPositionUpdater();
     }
     public void MidHorMoveRight()
     {
@@ -39,6 +145,8 @@ public class PlayerGridManager : MonoBehaviour
         playerGrid[1, 2] = playerGrid[1, 1];
         playerGrid[1, 1] = playerGrid[1, 0];
         playerGrid[1, 0] = temp;
+
+        GridPositionUpdater();
 
     }
     public void BotMoveRight()
@@ -48,31 +156,39 @@ public class PlayerGridManager : MonoBehaviour
         playerGrid[2, 1] = playerGrid[2, 0];
         playerGrid[2, 0] = temp;
 
+        GridPositionUpdater();
+
     }
 
     //move left
     public void TopMoveLeft()
     {
         bool temp = playerGrid[0, 0];
-        playerGrid[0, 1] = playerGrid[0, 2];
         playerGrid[0, 0] = playerGrid[0, 1];
+        playerGrid[0, 1] = playerGrid[0, 2];
         playerGrid[0, 2] = temp;
+
+        GridPositionUpdater();
 
     }
     public void MidHorMoveLeft()
     {
         bool temp = playerGrid[1, 0];
-        playerGrid[1, 1] = playerGrid[1, 2];
         playerGrid[1, 0] = playerGrid[1, 1];
+        playerGrid[1, 1] = playerGrid[1, 2];
         playerGrid[1, 2] = temp;
+
+        GridPositionUpdater();
 
     }
     public void BotMoveLeft()
     {
         bool temp = playerGrid[2, 0];
-        playerGrid[2, 1] = playerGrid[2, 2];
         playerGrid[2, 0] = playerGrid[2, 1];
+        playerGrid[2, 1] = playerGrid[2, 2];
         playerGrid[2, 2] = temp;
+
+        GridPositionUpdater();
 
     }
 
@@ -84,6 +200,8 @@ public class PlayerGridManager : MonoBehaviour
         playerGrid[1, 0] = playerGrid[2, 0];
         playerGrid[2, 0] = temp;
 
+        GridPositionUpdater();
+
     }
     public void MidVerMoveUp()
     {
@@ -91,6 +209,8 @@ public class PlayerGridManager : MonoBehaviour
         playerGrid[0, 1] = playerGrid[1, 1];
         playerGrid[1, 1] = playerGrid[2, 1];
         playerGrid[2, 1] = temp;
+
+        GridPositionUpdater();
 
     }
     public void LeftMoveUp()
@@ -100,15 +220,20 @@ public class PlayerGridManager : MonoBehaviour
         playerGrid[1, 2] = playerGrid[2, 2];
         playerGrid[2, 2] = temp;
 
+        GridPositionUpdater();
+
     }
 
     //move down
     public void RightMoveDown()
     {
-        bool temp = playerGrid[2, 0];
-        playerGrid[2, 0] = playerGrid[1, 0];
-        playerGrid[1, 0] = playerGrid[0, 0];
-        playerGrid[0, 0] = temp;
+        bool temp = playerGrid[2, 2];
+        playerGrid[2, 2] = playerGrid[1, 2];
+        playerGrid[1, 2] = playerGrid[0, 2];
+        playerGrid[0, 2] = temp;
+
+        GridPositionUpdater();
+
 
     }
     public void MidVerMoveDown()
@@ -118,13 +243,17 @@ public class PlayerGridManager : MonoBehaviour
         playerGrid[1, 1] = playerGrid[0, 1];
         playerGrid[0, 1] = temp;
 
+        GridPositionUpdater();
+
     }
     public void LeftMoveDown()
     {
-        bool temp = playerGrid[2, 2];
-        playerGrid[2, 2] = playerGrid[1, 2];
-        playerGrid[1, 2] = playerGrid[0, 2];
-        playerGrid[0, 2] = temp;
+        bool temp = playerGrid[2, 0];
+        playerGrid[2, 0] = playerGrid[1, 0];
+        playerGrid[1, 0] = playerGrid[0, 0];
+        playerGrid[0, 0] = temp;
+
+        GridPositionUpdater();
 
     }
 }
